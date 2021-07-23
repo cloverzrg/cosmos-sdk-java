a05d6a472c22ae04f55d09a91c18629f5f14364e
Zhu Rengen <zhurungen@wecash.net>
2021-07-23 13:22:22 +0800

    central maven

--- src/test/java/com/jeongen/cosmos/CosmosRestApiClientTest.java
+++ src/test/java/com/jeongen/cosmos/CosmosRestApiClientTest.java
@@ -1,15 +1,8 @@
 package com.jeongen.cosmos;
 
 import com.jeongen.cosmos.crypro.CosmosCredentials;
-import com.jeongen.cosmos.vo.SendInfo;
-import cosmos.base.abci.v1beta1.Abci;
-import cosmos.tx.v1beta1.ServiceOuterClass;
 import junit.framework.TestCase;
 import org.bouncycastle.util.encoders.Hex;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CosmosRestApiClientTest extends TestCase {
 
@@ -20,14 +13,14 @@ public class CosmosRestApiClientTest extends TestCase {
         CosmosCredentials credentials = CosmosCredentials.create(privateKey);
         // 获取地址
         System.out.println("address:" + credentials.getAddress());
-        List<SendInfo> sendList = new ArrayList<>();
-        sendList.add(SendInfo.builder().credentials(credentials).toAddress("cosmos12kd7gu4lamw29pv4u6ug8aryr0p7wm207uwt30").amountInAtom(new BigDecimal("0.0001")).build());
-        sendList.add(SendInfo.builder().credentials(credentials).toAddress("cosmos1u3zluamfx5pvgha0dn73ah4pyu9ckv6scvdw72").amountInAtom(new BigDecimal("0.0001")).build());
-        // 生成、签名、广播交易
-        Abci.TxResponse txResponse = cosmosRestApiClient.sendMultiTx(credentials, sendList, new BigDecimal("0.000001"), 200000);
-        System.out.println(txResponse);
-
-        // 获取指定高度的交易
-        ServiceOuterClass.GetTxsEventResponse txsEventByHeight = cosmosRestApiClient.getTxsEventByHeight(6900000L, "");
+//        List<SendInfo> sendList = new ArrayList<>();
+//        sendList.add(SendInfo.builder().credentials(credentials).toAddress("cosmos12kd7gu4lamw29pv4u6ug8aryr0p7wm207uwt30").amountInAtom(new BigDecimal("0.0001")).build());
+//        sendList.add(SendInfo.builder().credentials(credentials).toAddress("cosmos1u3zluamfx5pvgha0dn73ah4pyu9ckv6scvdw72").amountInAtom(new BigDecimal("0.0001")).build());
+//        // 生成、签名、广播交易
+//        Abci.TxResponse txResponse = cosmosRestApiClient.sendMultiTx(credentials, sendList, new BigDecimal("0.000001"), 200000);
+//        System.out.println(txResponse);
+//
+//        // 获取指定高度的交易
+//        ServiceOuterClass.GetTxsEventResponse txsEventByHeight = cosmosRestApiClient.getTxsEventByHeight(6900000L, "");
     }
 }
